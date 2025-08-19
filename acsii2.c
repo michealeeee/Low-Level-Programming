d708b631d36ff9a4f8d2f03c68a7c98dbb1ee292
mike <mke00007@gmail.com>
2025-08-19 05:55:45 +0000

    ascii using for loop

---
+++ acsii2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+int numbers;
+
+for (numbers = 41; numbers <= 90; numbers = numbers + 1)
+
+{
+	putchar(numbers);
+
+	putchar('\n');
+	
+
+
+}
+
+
+
+return 0;
+}
