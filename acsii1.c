d5ab8bdbe7f345f02d032a56072fd91661403686
mike <mke00007@gmail.com>
2025-08-19 05:32:14 +0000

    added a new ascii table

---
+++ acsii1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+
+int numbers = 41;
+
+while(numbers <= 90)
+
+{
+	putchar(numbers);
+	
+	putchar('\n');
+
+	numbers = numbers + 1;
+
+
+}
+
+
+
+return 0;
+}
---
+++ for_statement.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+	
+	int main()
+{
+	int score;
+	
+	printf("Enter a score to determine your grade:\n");
+	scanf("%d",&score);
+	printf("what is your score");
+	>= 85 Distinction
+	>= 75 A
+	>= 70 A+
+	>= 60 B
+	>= 50 C
+	>= 40 D
+	<= 40 F
+
+	printf("My grade is %d",score)
+
+
+return = 0
+
+}
